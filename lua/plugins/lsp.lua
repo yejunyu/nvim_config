8b69a7576b201d6d65e899e7ed4d9913a5559f6e
yjy <yyyejunyu@gmail.com>
2025-07-22 17:44:31 +0800

    init

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,5 +11,18 @@ return {
         }
       }
     }
+  },
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = { 'saghen/blink.cmp' },
+    -- example using `opts` for defining servers
+
+    -- example calling setup directly for each LSP
+    config = function()
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
+      local lspconfig = require('lspconfig')
+
+      lspconfig['lua_ls'].setup({ capabilities = capabilities })
+    end
   }
 }
